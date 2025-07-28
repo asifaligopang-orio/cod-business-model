91d94ca8e397ae69c41074b75c74b5f89ed7810c
asifaligopang-orio <115686243+asifaligopang-orio@users.noreply.github.com>
2025-07-28 15:15:49 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CODBusinessModel from './CODBusinessModel';
+
+function App() {
+  return (
+    <div className="App">
+      <CODBusinessModel />
+    </div>
+  );
+}
+
+export default App;
